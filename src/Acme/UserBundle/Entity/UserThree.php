937affa0bfd7e11fca56ab340c04424c0aa56f6e
A-med <ahmeddammak91@gmail.com>
2015-09-11 03:46:58 +0200

    Update UserThree.php

--- src/Acme/UserBundle/Entity/UserThree.php
+++ src/Acme/UserBundle/Entity/UserThree.php
@@ -7,7 +7,7 @@ use PUGX\MultiUserBundle\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="user_two")
+ * @ORM\Table(name="user_three")
  * @UniqueEntity(fields = "username", targetClass = "Acme\UserBundle\Entity\User", message="fos_user.username.already_used")
  * @UniqueEntity(fields = "email", targetClass = "Acme\UserBundle\Entity\User", message="fos_user.email.already_used")
  */
